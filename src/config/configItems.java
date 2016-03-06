a5e2997afacfa6dc0a19f7777aa0d3da60f9e5c7
Dan Figueras <blad3dancer@gmail.com>
2016-03-06 11:30:07 +0100

    After setting everything in IntelliJ

---
+++ src/config/configItems.java
@@ -0,0 +1,11 @@
+package config;
+
+/**
+ * Created by Dan Figueras on 06/03/2016.
+ * Belongs to project routecalculator.
+ * All rights reserved.
+ */
+public final class configItems {
+    public static String environment="Loc";
+
+}
